589a7555d416b3bea914b8abc486c1af98508da1
Aarav <aarav@Mehuls-MacBook-Pro.local>
2020-06-07 11:58:24 +0530

    c 27 angry birds

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
@@ -15,17 +16,18 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-
+    
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
-    pig1 = new Pig(810, 350);
-    log1 = new Log(810,260,300, PI/2);
-
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
+
+    pig1 = new Pig(810, 350);
+    log1 = new Log(810,260,300, PI/2);
+    
     pig3 = new Pig(810, 220);
 
     log3 =  new Log(810,180,300, PI/2);
@@ -33,8 +35,11 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
+    log6 = new Log(230, 180, 80, PI/2);
 
-    bird = new Bird(100,100);
+    bird1 = new Bird(100,100);
+
+    Chain1 = new chain(bird1.body, log6.body);
 
 }
 
@@ -58,7 +63,10 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+    log6.display();
 
-    bird.display();
+    bird1.display();
     platform.display();
+
+    Chain1.display();
 }
